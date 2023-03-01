e96d9d575b9a92a07b22e4ff8db84264cc381888
Robson Ferreira dos Santos Junior <robson.santos.rs27@gmail.com>
2023-03-01 14:30:44 -0300

    1.19. Testando o controlador modelo

--- app/Http/Controllers/ModeloController.php
+++ app/Http/Controllers/ModeloController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
+use Illuminate\Support\Facades\Storage;
+
 class ModeloController extends Controller
 {
     public function __construct(Modelo $modelo){
@@ -76,8 +78,10 @@ class ModeloController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Modelo $modelo)
+    public function update(Request $request, $id)
     {
+        $modelo = $this->modelo->find($id);
+
         if($modelo === null){
             return response()->json(['erro' => 'O recurso pesquisado nao existe. Impossivel atualizar.'], 404);
         }
@@ -133,6 +137,6 @@ class ModeloController extends Controller
         Storage::disk('public')->delete($modelo->imagem);
 
         $modelo->delete();
-        return ['msg' => 'O modelo foi removida com sucesso!'];
+        return ['msg' => 'O modelo foi removido com sucesso!'];
     }
 }
