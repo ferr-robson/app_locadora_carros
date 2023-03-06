8a7f8d0c2b9a736a14918c805b067c21748dd528
Robson Ferreira dos Santos Junior <robson.santos.rs27@gmail.com>
2023-03-06 17:26:56 -0300

    2.4. Criando as rotas e controlador auth

--- routes/api.php
+++ routes/api.php
@@ -28,3 +28,9 @@ Route::apiResource('carro', 'App\Http\Controllers\CarroController');
 Route::apiResource('locacao', 'App\Http\Controllers\LocacaoController');
 Route::apiResource('marca', 'App\Http\Controllers\MarcaController');
 Route::apiResource('modelo', 'App\Http\Controllers\ModeloController');
+
+//Route::post('login', 'App\Http\Controllers\AuthController@login');
+Route::post('login', [\App\Http\Controllers\AuthController::class,'login']);
+Route::post('logout', [\App\Http\Controllers\AuthController::class,'logout']);
+Route::post('refresh', [\App\Http\Controllers\AuthController::class,'refresh']);
+Route::post('me', [\App\Http\Controllers\AuthController::class,'me']);
