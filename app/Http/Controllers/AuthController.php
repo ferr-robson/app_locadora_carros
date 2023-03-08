05d1d5122569b35aead2b47be99c97d586195d53
Robson Ferreira dos Santos Junior <robson.santos.rs27@gmail.com>
2023-03-08 13:24:57 -0300

    2.5. Autenticacao JWT

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,8 +6,18 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login() {
-        return 'login';
+    public function login(Request $request) {
+        $credenciais = $request->all(['email', 'password']);
+        
+        // autenticacao (email e senha)
+        // auth(<config\auth.php valor do vetor guards>)
+        $token = auth('api')->attempt($credenciais);
+        if($token){
+            // retornar o token
+            return response()->json(['token' => $token], 200);
+        } else {
+            return response()->json(['erro' => 'Usuário ou senha inválido!'], 403);
+        }
     }
 
     public function logout() {
