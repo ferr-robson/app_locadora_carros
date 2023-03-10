6fb5c9319226e4eb1745b8796193e755939fe82a
Robson Ferreira dos Santos Junior <robson.santos.rs27@gmail.com>
2023-03-10 13:50:37 -0300

    3.10. Criando card de listagem de registros

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -2,37 +2,83 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-
-                <div class="card">
-
-                    <div class="card-header">Busca de marcas</div>
-
+                <!-- Card de busca -->
+                <div class="card mb-3">
+                    <!-- Card header -->
+                    <div class="card-header">Busca de marcas</div><!-- /Card header -->
+                    
+                    <!-- Card body -->
                     <div class="card-body">
+                        <!-- .row -->
                         <div class="row">
+                            <!-- .col -->
                             <div class="col mb-3">
-
+                                <!-- input-container-component -->
                                 <input-container-component id="inputId" titulo="ID" texto-ajuda="Opcional. Informe o ID do registro" id-help="idHelp">
                                     <input type="number" class="form-control" id="inputID" aria-describedby="idHelp" placeholder="ID">
-                                </input-container-component>
-
-                                
-                                
-                            </div>
+                                </input-container-component><!-- /input-container-component -->
+                            </div><!-- /.col -->
+                            
+                            <!-- .col -->
                             <div class="col mb-3">
-
+                                <!-- input-container-component -->
                                 <input-container-component id="inputNome" titulo="Nome" texto-ajuda="Opcional. Informe o Nome da marca" id-help="nomeHelp">
                                     <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp" placeholder="Nome">
-                                </input-container-component>
-                            </div>
-                        </div>
+                                </input-container-component><!-- /input-container-component -->
+                            </div><!-- /.col -->
+                        </div> <!-- /.row -->
+                    </div><!-- /Card body -->
 
-                    </div>
-
+                    <!-- Card footer -->
                     <div class="card-footer">
                         <button type="submit" class="btn btn-primary btn-sm float-end">Pesquisar</button>
-                    </div>
-                </div>
+                    </div><!-- /Card footer -->
+                </div><!-- /Card de busca -->
 
+                <!-- Card listagem de marcas -->
+                <div class="card">
+                    <!-- Card header -->
+                    <div class="card-header">Relação de marcas</div><!-- /Card header -->
+                    
+                    <!-- Card body -->
+                    <div class="card-body">
+                        <!-- .table -->
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Handle</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                                </tr>
+                                <tr>
+                                <th scope="row">3</th>
+                                <td colspan="2">Larry the Bird</td>
+                                <td>@twitter</td>
+                                </tr>
+                            </tbody>
+                        </table><!-- /.table -->
+                    </div><!-- /Card body -->
+
+                    <!-- Card footer -->
+                    <div class="card-footer">
+                        <button type="button" class="btn btn-primary btn-sm float-end">Adicionar</button>
+                    </div><!-- /Card footer -->
+                </div><!-- /Card de listagem de marcas -->
             </div>
         </div>
     </div>
