836f1e9c12a74a6ec769e3e323ab83a4ab6e4377
Robson Ferreira dos Santos Junior <robson.santos.rs27@gmail.com>
2023-03-16 13:13:21 -0300

    3.21. Listando os dados de marca na tabela

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -32,7 +32,7 @@
 
                 <card-component titulo="Relação de marcas">
                     <template v-slot:conteudo>
-                        <table-component></table-component>
+                        <table-component :dados="marcas" :titulos="['ID', 'Nome', 'Imagem']"></table-component>
                     </template>
                     <template v-slot:rodape>
                         <button type="button" class="btn btn-primary btn-sm float-end" data-bs-toggle="modal" data-bs-target="#modalMarca">Adicionar</button>
@@ -100,7 +100,6 @@
                 axios.get(this.urlBase, config)
                     .then(response => {
                         this.marcas = response.data;
-                        console.log(this.marcas)
                     })
                     .catch(errors => {
                         console.log(errors);
