6ee41dba4165f41fc8df4b87be5dafb0b15a7cd6
Robson Ferreira dos Santos Junior <robson.santos.rs27@gmail.com>
2023-03-16 14:41:45 -0300

    3.22. Transformando a tebela em tabela dinamica

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -32,7 +32,7 @@
 
                 <card-component titulo="Relação de marcas">
                     <template v-slot:conteudo>
-                        <table-component :dados="marcas" :titulos="['ID', 'Nome', 'Imagem']"></table-component>
+                        <table-component :dados="marcas" :titulos="['id', 'nome', 'imagem']"></table-component>
                     </template>
                     <template v-slot:rodape>
                         <button type="button" class="btn btn-primary btn-sm float-end" data-bs-toggle="modal" data-bs-target="#modalMarca">Adicionar</button>
--- resources/js/components/Table.vue
+++ resources/js/components/Table.vue
@@ -3,16 +3,21 @@
     <table class="table table-hover">
         <thead>
             <tr>
-                <th scope="col" v-for="titulo, key in titulos" :key="key">{{titulo}}</th>
+                <th scope="col" v-for="titulo, key in titulos" :key="key" class="text-uppercase">{{titulo}}</th>
             </tr>
         </thead>
         <tbody>
-            <tr v-for="dado in dados" :key="dado.id">
-                <th scope="row">{{dado.id}}</th>
-                <td>{{dado.nome}}</td>
-                
-                <!-- recuperando as imagens -->
-                <td><img :src="'/storage/'+dado.imagem" width="30" height="30"></td>
+            <tr v-for="obj in dados" :key="obj.id">
+                <template v-for="(valor, chave) in obj">
+                    <td v-if="titulos.includes(chave)" :key="chave">
+                        <span v-if="chave != 'imagem'">
+                            {{valor}}
+                        </span>
+                        <span v-else>
+                            <img :src="'/storage/'+valor" width="30" height="30">
+                        </span>
+                    </td>
+                </template>
             </tr>
         </tbody>
     </table><!-- /.table -->
