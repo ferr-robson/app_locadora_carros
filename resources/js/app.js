3bc0ab29b43610f61b45fc778c7e923ecd9af144
Robson Ferreira dos Santos Junior <robson.santos.rs27@gmail.com>
2023-03-17 15:50:45 -0300

    3.32. Bainxando e configurando o Vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,16 @@
 import './bootstrap';
 import { createApp } from 'vue';
 
+// Import do Vuex
+import { createStore } from 'vuex'
+
+// Criacao de uma nova store
+const store = createStore({
+    state: {
+        teste: 'Teste de recuperacao de valor da Store Vuex'
+    }
+})
+
 /**
  * Next, we will create a fresh Vue application instance. You may then begin
  * registering components with the application instance so they are ready
@@ -14,6 +24,8 @@ import { createApp } from 'vue';
  */
 
 const app = createApp({});
+
+app.use(store);
 
 import ExampleComponent from './components/ExampleComponent.vue';
 app.component('example-component', ExampleComponent);
--- resources/js/components/Table.vue
+++ resources/js/components/Table.vue
@@ -1,35 +1,38 @@
 <template>
-    <!-- .table -->
-    <table class="table table-hover">
-        <thead>
-            <tr>
-                <th scope="col" v-for="t, key in titulos" :key="key">{{t.titulo}}</th>
-                <th v-if="visualizar.visivel || atualizar || remover"></th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="obj, chave in dadosFiltrados" :key="chave">
-                <td v-for="valor, chaveValor in obj" :key="chaveValor">
-                    <span v-if="titulos[chaveValor].tipo == 'texto'">
-                        {{valor}}
-                    </span>
+    <div>
+        {{$store.state.teste}}
+        <!-- .table -->
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col" v-for="t, key in titulos" :key="key">{{t.titulo}}</th>
+                    <th v-if="visualizar.visivel || atualizar || remover"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="obj, chave in dadosFiltrados" :key="chave">
+                    <td v-for="valor, chaveValor in obj" :key="chaveValor">
+                        <span v-if="titulos[chaveValor].tipo == 'texto'">
+                            {{valor}}
+                        </span>
 
-                    <span v-if="titulos[chaveValor].tipo == 'data'">
-                        {{valor}}
-                    </span>
+                        <span v-if="titulos[chaveValor].tipo == 'data'">
+                            {{valor}}
+                        </span>
 
-                    <span v-if="titulos[chaveValor].tipo == 'imagem'">
-                        <img :src="'/storage/'+valor" width="30" height="30">
-                    </span>
-                </td>
-                <td v-if="visualizar.visivel || atualizar || remover">
-                    <button v-if="visualizar.visivel" class="btn btn-outline-primary btn-sm" :data-bs-toggle="visualizar.dataBSToggle" :data-bs-target="visualizar.dataBSTarget">Visualizar</button>
-                    <button v-if="atualizar" class="btn btn-outline-primary btn-sm">Atualizar</button>
-                    <button v-if="remover" class="btn btn-outline-danger btn-sm">Remover</button>
-                </td>
-            </tr>
-        </tbody>
-    </table><!-- /.table -->
+                        <span v-if="titulos[chaveValor].tipo == 'imagem'">
+                            <img :src="'/storage/'+valor" width="30" height="30">
+                        </span>
+                    </td>
+                    <td v-if="visualizar.visivel || atualizar || remover">
+                        <button v-if="visualizar.visivel" class="btn btn-outline-primary btn-sm" :data-bs-toggle="visualizar.dataBSToggle" :data-bs-target="visualizar.dataBSTarget">Visualizar</button>
+                        <button v-if="atualizar" class="btn btn-outline-primary btn-sm">Atualizar</button>
+                        <button v-if="remover" class="btn btn-outline-danger btn-sm">Remover</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table><!-- /.table -->
+    </div>
 </template>
 
 <script>
